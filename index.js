a17ba3c450038cb2f26533696a1e2fec609e776c
Sanket Patel <sanketpatel1996@hotmail.com>
2021-02-02 15:34:43 -0500

    CORS settings and listening to request from frontend

--- index.js
+++ index.js
@@ -2,17 +2,22 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
-// Change for production 
 const port = process.env.port || 5000;
 
 const rotuer = require('./router')
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server, {
+    cors: {
+        origin: 'http://localhost:3000'
+    }
+});
 
 io.on('connection', (socket) => {
     console.log('We have a new connection.');
+
+    socket.on('name', console.log)
 
     socket.on('disconnect', () => {
         console.log('User has left.');
