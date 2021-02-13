1c1368bfb3d23c75da69ba3b8ffbda953389d558
Sanket Patel <sanketpatel1996@hotmail.com>
2021-02-12 19:29:38 -0500

    added socket.io support

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users')
+
 const port = process.env.port || 5000;
 
-const rotuer = require('./router')
+const rotuer = require('./router');
+const { callbackify } = require('util');
 
 const app = express();
 const server = http.createServer(app);
@@ -15,9 +18,26 @@ const io = socketio(server, {
 });
 
 io.on('connection', (socket) => {
-    console.log('We have a new connection.');
 
-    socket.on('name', console.log)
+    socket.on('join', ({ name, room }, useCallback) => {
+        console.log(`Name: ${name}  Room: ${room}`);
+        const { error, user } = addUser( { id: socket.id, name, room});
+
+        if(error) return useCallback(error);
+
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room ${user.room}`});
+        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name}, has joined!`}); 
+
+        socket.join(user.room);
+
+    });
+    
+    socket.on('sendMessage', (message, useCallback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', { user: user.name, text: message});
+    
+    });
 
     socket.on('disconnect', () => {
         console.log('User has left.');
